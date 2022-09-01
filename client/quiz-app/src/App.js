ea632cb09c3c968c9f314b75ecb2cfa134c0e586
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-09-01 19:35:56 +0200

    Landingpage

--- client/quiz-app/src/App.js
+++ client/quiz-app/src/App.js
@@ -1,23 +1,22 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Landingpage from "./pages/Landingpage";
 import Dashboard from "./pages/Dashboard";
 import Blogposts from "./pages/Blogposts";
 import Quizgame from "./pages/Quizgame";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/Signup" element={<Signup/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/Blogposts" element={<Blogposts/>}/>
-        <Route path="/Quizgame" element={<Quizgame/>}/>
-        
-        
+        <Route path="/" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/landingpage" element={<Landingpage />} />
+        <Route path="/Blogposts" element={<Blogposts />} />
+        <Route path="/Quizgame" element={<Quizgame />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
---
+++ client/quiz-app/src/pages/Landingpage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+
+export default function Landingpage() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+
+
+        <Link to="/Quizgame"><div className='bg-white w-full h-full object-cover pt-60 pb-60 border-gray-200 border-4 p-8'>
+        <div><h2 className='text-4xl font-bold text-center py-6'>WANNA PLAY A GAME?</h2></div>
+      </div></Link>
+      
+      <Link to="/Blogposts"><div className='bg-white w-full h-full object-cover pt-60 pb-60 border-gray-200 border-4 p-8'>
+        <h2 className='text-4xl font-bold text-center py-6'>DISCUSS WITH MEMBERS?</h2>
+        </div></Link>
+
+    </div>
+  )
+}
