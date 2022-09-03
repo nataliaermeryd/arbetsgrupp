2386925962bff0fec87f65f8ff0eb64e2631d63f
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-09-03 14:27:28 +0200

    CSS Commits

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom';
 
-import loginImg from '../assets/Login.jpeg'
-
 export default function Login() {
     const navigate = useNavigate();
     const navigateToSignup = () => {
@@ -11,32 +9,29 @@ export default function Login() {
     
   return (
 
-    
+    <section class='fixed top-0 left-0 right-0 bottom-0 flex items-center w-full h-full p-4 bg-coolGray-800 bg-opacity-80'>
 
+        <div class='max-w-lg w-full m-auto p-8 bg-white rounded-2xl'>
+            <form class='p-7 mb-8 bg-coolGray-50 rounded-2xl border-2 border-dashed gray-200 border-4 text-center'>
+                  <h2 class='text-3xl text-gray-400 font-bold text-center py-6'>| THE DISCUSSING QUIZ |</h2>
+                  
+                <div class='flex flex-col py-2'>
+                    <input class='border p-2 rounded-xl text-center' type="text" placeholder='USERNAME' autocomplete='off' />
+                  </div>
 
-    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-        <div className='hidden sm:block'>
-            <img className='w-full h-full object-cover' src={loginImg} alt="" />
-        </div>
+                <div class='flex flex-col py-2'>
+                    <input class='border p-2 rounded-xl text-center' type="text" placeholder='PASSWORD' autocomplete='off' />
+                  </div>
 
-        <div className='bg-orange-500 flex flex-col justify-center'>
-            <form className='max-w-[400px] w-full mx-auto bg-blue p-4'>
-                <h2 className='text-4xl font-bold text-center py-6'>THE DISCUSSING QUIZ</h2>
-                <div className='flex flex-col py-2'>
-                    <label>Username</label>
-                    <input className='border p-2' type="text" />
+                    <div class='flex justify-between'>
+                    <p class='flex items-center'><input class='mr-2' type="checkbox" /> Remember Me</p>
+                    <button type='click' onClick={navigateToSignup} >Sign Up!</button>
                 </div>
-                <div className='flex flex-col py-2'>
-                    <label>Password</label>
-                    <input className='border p-2' type="password" />
-                </div>
-                <button className='border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white'>Sign In</button>
-                <div className='flex justify-between'>
-                    <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
-                    <button type='click' onClick={navigateToSignup} >Skapa konto</button>
-                </div>
+                  <button class='border rounded-xl w-full my-5 py-2 bg-indigo-500 hover:bg-indigo-200 text-white'><b>SIGN IN</b></button>
+                  <button class='border rounded-xl w-full my-5 py-2 bg-red-500 hover:bg-red-100 text-white'><b>SIGN IN WITH GOOGLE</b></button>
             </form>
-        </div>
-    </div>
+        </div>  
+          
+    </section>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,30 +1,61 @@
 import React from 'react'
-
+import {useNavigate} from 'react-router-dom';
 
 
 export default function Signup() {
-  return (
-    <div className='grid grid-cols-1 h-screen w-full'>
+        const navigate = useNavigate();
+        const navigateToSignin = () => {
+        navigate('/');
+    };
+    return (
+      
+    <section class='fixed top-0 left-0 right-0 bottom-0 flex items-center w-full h-full p-4 bg-coolGray-800 bg-opacity-80'>
 
+        <div class='max-w-lg w-full m-auto p-8 bg-white rounded-2xl'>
+                <form method='post' class='p-7 mb-8 bg-coolGray-50 rounded-2xl border-2 border-dashed gray-200 border-4'>
+                <p class='text-bold text-gray-400 text-2xl cursor-pointer' type='click' onClick={navigateToSignin} >⟪</p>
+                  <h2 class='text-3xl text-gray-400 font-bold text-center py-6'>| SIGN UP |</h2>
+                  
+                <div class='flex flex-col py-2'>
+                        <input
+                            class='border p-2 rounded-xl text-center'
+                            type="email"
+                            placeholder='YOUR EMAIL'
+                            autocomplete='off'
 
-        <div className='bg-orange-500 flex flex-col justify-center'>
-            <form className='max-w-[400px] w-full mx-auto bg-blue p-4'>
-                <h2 className='text-4xl font-bold text-center py-6'>Signup</h2>
-                <div className='flex flex-col py-2'>
-                    <label>Username</label>
-                    <input className='border p-2' type="text" />
-                </div>
-                <div className='flex flex-col py-2'>
-                    <label>Password</label>
-                    <input className='border p-2' type="password" />
-                </div>
-                <button className='border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white'>Sign In</button>
-                <div className='flex justify-between'>
-                    <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
-                    <button type='click'>Skapa konto</button>
-                </div>
+                            id='email'
+                            required="REQUIRED"
+                        />
+                  </div>
+
+                <div class='flex flex-col py-2'>
+                        <input
+                            class='border p-2 rounded-xl text-center'
+                            type="text"
+                            placeholder='NEW USERNAME'
+                            autocomplete='off'
+
+                            id='username'
+                            required="REQUIRED"
+                        />
+                  </div>
+
+                <div class='flex flex-col py-2'>
+                        <input
+                            class='border p-2 rounded-xl text-center'
+                            type="text"
+                            placeholder='NEW PASSWORD'
+                            autocomplete='off'
+
+                            id='password'
+                            required="REQUIRED"
+                        />
+                  </div>
+                  <button type='submit' class='border rounded-xl text-bold w-full my-5 py-2 bg-indigo-500 hover:bg-indigo-200 text-white'><b>SIGN UP</b></button>
+                  <button class='border rounded-xl text-bold w-full my-5 py-2 bg-red-500 hover:bg-red-100 text-white'><b>SIGN UP WITH GOOGLE</b></button>
             </form>
-        </div>
-    </div>
+        </div>  
+          
+    </section>
   )
 }
