a83eee3364dfd64e439bdad8cbf672b8e6186993
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-09-28 09:45:07 +0200

    footer

--- src/P-Components/Footer.jsx
+++ src/P-Components/Footer.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-
+import React from "react";
 
 export default function Footer() {
   return (
@@ -11,34 +9,52 @@ export default function Footer() {
       shadow md:flex md:items-center md:justify-between md:p-6 
       dark:
       dark:
-      ">
+      "
+    >
       <span
         class="
         text-sm sm:text-center text-transparent bg-clip-text 
                   bg-gradient-to-r from-blue-600 to-purple-300
-        ">
-        © 2022 <a href="/" class="text-transparent bg-clip-text 
-                  bg-gradient-to-r to-red-500 from-pink-200 hover:text-[15px]">SPERN™</a>. All Rights Reserved.
-    </span>
+        "
+      >
+        © 2022{" "}
+        <a
+          href="/"
+          class="text-transparent bg-clip-text 
+                  bg-gradient-to-r to-red-500 from-pink-200 hover:text-[15px]"
+        >
+          SP'RN™
+        </a>
+        . All Rights Reserved.
+      </span>
       <ul
         class="
         flex flex-wrap items-center mt-3 text-sm mt-0
         text-transparent bg-clip-text 
         bg-gradient-to-r from-blue-600 via-purple-300 via-pink-300 to-red-400
-        ">
+        "
+      >
         <li>
-            <a href="#" class="mr-4 hover:text-[15px] md:mr-6 ">LINK</a>
+          <a href="#" class="mr-4 hover:text-[15px] md:mr-6 ">
+            LINK
+          </a>
         </li>
         <li>
-            <a href="#" class="mr-4 hover:text-[15px] md:mr-6">LOREM</a>
+          <a href="#" class="mr-4 hover:text-[15px] md:mr-6">
+            LOREM
+          </a>
         </li>
         <li>
-            <a href="#" class="mr-4 hover:text-[15px] md:mr-6">LINK</a>
+          <a href="#" class="mr-4 hover:text-[15px] md:mr-6">
+            LINK
+          </a>
         </li>
         <li>
-            <a href="#" class="hover:text-[15px]">LOREM</a>
+          <a href="#" class="hover:text-[15px]">
+            LOREM
+          </a>
         </li>
-    </ul>
-</footer>
+      </ul>
+    </footer>
   );
-};
+}
