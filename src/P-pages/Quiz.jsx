fa8746030fe71dfb6ba6c5e59026d76317db587e
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-09-30 14:07:13 +0200

    Quiz.test

--- src/P-pages/Quiz.jsx
+++ src/P-pages/Quiz.jsx
@@ -1,37 +1,30 @@
 import { useNavigateBack } from "../P-Hooks/useNavigateBack";
 
 const Quiz = () => {
-  
   const { navigateBack } = useNavigateBack();
-  
 
-return (   
-  
-<section className="quizPage pt-[90px]">
-        <p
-          class='absolute text-left pl-3 pt-2
+  return (
+    <section className="quizPage pt-[90px]">
+      <paragraph
+        class="absolute text-left pl-3 pt-2
           font-bold text-gray-400 text-[24px]
           cursor-pointer
-          hover:text-[26px] hover:text-gray-300'
-          type='click'
-          onClick={ navigateBack }
-        >
-          ⟪
-        </p>
+          hover:text-[26px] hover:text-gray-300"
+        type="click"
+        onClick={navigateBack}
+      >
+        ⟪
+      </paragraph>
 
-  <div
-    id="quizCommingSoon"
-    class="
-    ">
-
-      <h1 class="text-6xl text-white underline">
-        Comming Soon
-      </h1>
-           
-     </div>
-</section>            
-        
-    )
-}
+      <div
+        id="quizCommingSoon"
+        class="
+    "
+      >
+        <h1 class="text-6xl text-white underline">Comming Soon</h1>
+      </div>
+    </section>
+  );
+};
 
 export default Quiz;
